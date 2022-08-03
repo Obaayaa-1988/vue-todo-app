35692a3ffb341069f80da79d43a94255e55867c5
Abigail <ashbella333@gmail.com>
2022-08-03 17:08:58 +0000

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NavBar />
+    <router-view />
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
+<script type="text/javascript">
+import NavBar from "@/components/NavBar.vue";
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    NavBar, // register component
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <div>
+      <nav>
+        <ul>
+          <li><router-link to="/">Home</router-link></li>
+          <li><router-link to="/">Blog</router-link></li>
+          <li><router-link to="about">About</router-link></li>
+        </ul>
+      </nav>
+      <h1 class="bg-gray-400 w-24 h-14">Hellloooooooo</h1>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>s
