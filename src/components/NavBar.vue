afa2bd4b6a2aa1294dbd87f7f20ff23c9046ba32
Abigail <ashbella333@gmail.com>
2022-08-10 16:36:04 +0000

    added cursor pointer to links

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,13 +3,13 @@
     <div>
       <nav>
         <ul class="text-center">
-          <li class="inline-block pl-8">
+          <li class="inline-block pl-8 cursor-pointer">
             <router-link to="/">Home</router-link>
           </li>
-          <li class="inline-block pl-8">
+          <li class="inline-block pl-8 cursor-pointer ">
             <router-link to="/">Blog</router-link>
           </li>
-          <li class="inline-block pl-8">
+          <li class="inline-block pl-8 cursor-pointer ">
             <router-link to="about">About</router-link>
           </li>
         </ul>
--- src/components/TodoLists.vue
+++ src/components/TodoLists.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="hello">
-    <div
-      class="bg-image-backs w-screen h-screen bg-no-repeat bg-center bg-cover"
-    >
+    <div class="bg-image-backs w-screen h-screen bg-no-repeat bg-center bg-cover" >
+         <NavBar />
     <div class="pt-40">
 
         <div class=" bg-red-200 h-2/4 w-3/6 rounded-lg ml-auto mr-auto  text-white py-8">
-  
+
+            
           <input type="text" placeholder="add a todo" class="text-black"/>
           <h1>heloooooo hellllooo</h1>
           <h1>heloooooo hellllooo</h1>
@@ -26,11 +26,11 @@
 </template>
 
 <script>
-// import NavBar from "./NavBar.vue";
+import NavBar from "./NavBar.vue";
 export default {
   name: "TodoList",
   components: {
-    // NavBar,
+    NavBar,
   },
 };
 </script>
