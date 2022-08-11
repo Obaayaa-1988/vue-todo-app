38a302f78aa929f347aaef3fabc1b68d8d3326fc
Abigail <ashbella333@gmail.com>
2022-08-11 14:59:54 +0000

    data passed from app.vue to todoItem.vue

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,17 +1,23 @@
 <template>
-  
+  <div>
+    <!---using the data passed from the app.vue todos here-->
+    <p>{{ todoArray.completed }}</p>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "TodoItem",
-    components:{
-        
-    }
+  name: "TodoItem",
+  components: {},
+  //   props: {
+  //     todoArray: String,
+  //   },
 
-}
+  //data from the app.vue sent to todolist.vue now to todoitem component prop drilling data from grandmother to mother to child
+  //
+  props: ["todoArray"],
+};
 </script>
 
 <style>
-
 </style>
--- src/components/TodoLists.vue
+++ src/components/TodoLists.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="hello">
-    <div class="bg-image-backs w-screen h-screen bg-no-repeat bg-center bg-cover" >
-         <NavBar />
+    <!-----since we have received the todos array data from app.vue we loop through it to get each item in the array, we can now send eact item as prop to the
+    the todoItem component the :todoTask is just the variable name for the prop, todoArray is representing each todo item inside the todoArray-->
+    <NavBar />
+
+    <div>
+      <h2>My todolist</h2>
+      <ul>
+        <li v-for="todo in todosArray" :key="todo.id" class="text-red-500">
+          <TodoItem :todoArray="todo" />
+        </li>
+      </ul>
+    </div>
+    <!-- <div class="bg-image-backs w-screen h-screen bg-no-repeat bg-center bg-cover" >
+        
     <div class="pt-40">
 
         <div class=" bg-red-200 h-2/4 w-3/6 rounded-lg ml-auto mr-auto  text-white py-8">
@@ -21,17 +33,25 @@
         </div>
     </div>
       
-    </div>
+    </div> -->
   </div>
 </template>
 
 <script>
 import NavBar from "./NavBar.vue";
+import TodoItem from "./TodoItem.vue";
 export default {
   name: "TodoList",
   components: {
     NavBar,
+    TodoItem,
   },
+
+  // props: {
+  //   todosArray: Array,
+  // },
+
+  props: ["todosArray"],
 };
 </script>
 
