2c43c8bc256c3ba8873108fd860a1cc4a90952e8
Abigail <ashbella333@gmail.com>
2022-10-11 16:53:58 +0000

    added add todo function to parent component

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
     <!----This where we are transferring the todos array data to the Todolist Component and then transfer to the todoItem component-->
     <div class="bg-red-300 w-screen min-h-screen overflow-hidden">
       <NavBar />
-      <AddTodo v-on:add-task="addTask" />
-      <TodoLists :todosArray="todosArray" v-on:delete-task="deleteTask" />
+      <AddTodo v-on:add-todo="addTodos" />
+      <TodoLists v-on:delete-task="deleteTask" />
       
     </div>
   </div>
@@ -29,18 +29,12 @@ export default {
 
   data() {
     return {
-      todosArray: [
-        { id: 1, task: "Visit Germany and visit friends", completed: false },
-        { id: 2, task: "Visit Canada and visit friends", completed: false },
-        { id: 3, task: "Visit Armsterdam", completed: false },
-        { id: 4, task: "Visit USA", completed: false },
-        { id: 5, task: "Visit Norway", completed: false },
-      ],
+      
     };
   },
   methods: {
-    addTask(newTask) {
-      this.todosArray = [...this.todosArray, newTask]; //newTask parameter is from the parameter passed for the event(emit) in addTodo Component
+    addTodos() {
+      this.todosArray = [...this.todosArray, ]; //newTask parameter is from the parameter passed for the event(emit) in addTodo Component
       // console.log("jsjjdjjdjjj", newTask);
     },
 
@@ -48,6 +42,7 @@ export default {
       this.todosArray = this.todosArray.filter((todo) => todo.id !== taskId);
       console.log("mmdmdmmaaamm", taskId)
     },
+
 
     
   },
